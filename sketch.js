730e1ddc6471c6375ae39d796f13dd7591a59cae
Eliminator1357 <eliminator1357@gmail.com>
2023-06-06 13:01:30 +0200

    Flowchart toegevoegd

--- sketch.js
+++ sketch.js
@@ -4,13 +4,14 @@ var targetInput;
 var target;
 var nextStep;
 var numArray = [];
+
 function setup() {
   const canvascontainer = document.getElementById('canvascontainer');
 
   canvas = createCanvas(arraySize, arraySize);
   canvas.background(0);
   canvas.parent(canvascontainer);
-  
+
   const button = document.getElementById('start');
   button.addEventListener('click', start);
 
@@ -23,10 +24,10 @@ function validInputCheck() {
   if (target === "") {
     target = Math.floor(Math.random() * arraySize);
   }
-  if (typeof(target) ===  'string') {
+  if (typeof (target) === 'string') {
     target = parseInt(target);
   }
-  if (target > arraySize-1) {
+  if (target > arraySize - 1) {
     target = arraySize - 1;
   } else if (target < 0) {
     target = 0;
@@ -42,8 +43,17 @@ function start() {
   draw_values(numArray, target, target);
 
   const targetIndex = binarySearch(numArray, target);
-  alert(numArray[targetIndex]);
+  //alert(numArray[targetIndex]);
+  draw_values(numArray, target, target);
+
+  const result = document.getElementById('result');
+  console.log(targetIndex);
+  result.innerHTML = targetIndex;
 }
+
+
+
+/* Deze functie ga ik veranderen */
 
 function binarySearch(numArray, targetnum) {
   var left = 0;
@@ -51,6 +61,8 @@ function binarySearch(numArray, targetnum) {
   while (left <= right) {
     var middle = Math.floor((left + right) / 2);
     var middlenum = numArray[middle]
+    draw_values(numArray, left, right);
+
     if (middlenum === targetnum) {
       return middle;
     } else if (middlenum < targetnum) {
@@ -58,11 +70,19 @@ function binarySearch(numArray, targetnum) {
     } else {
       right = middle - 1;
     }
-    
+
   }
 }
 
+
+
+
+
+
+
+
 function generateArray() {
+  numArray = [];
   for (let i = 0; i < arraySize; i++) {
     numArray.push(i);
   }
@@ -72,9 +92,9 @@ function draw_values(values, left, right) {
   values.forEach((value) => {
 
     if (value >= left && value <= right) {
-      stroke(0,255,0);
+      stroke(0, 255, 0);
     } else {
-      stroke(255,255,255);
+      stroke(255, 255, 255);
     }
     line(value, arraySize, value, arraySize - value); // x1 y1 x2 y2
   });
